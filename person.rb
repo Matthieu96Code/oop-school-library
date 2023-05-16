96d4ce0ab3d3ae1ca5b47a6d2440bd49cdbd6748
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-16 08:18:29 +0000

    Set getters and setters

--- person.rb
+++ person.rb
@@ -1,6 +1,11 @@
 class Person
+  
+  attr_reader :id
+  attr_accessor :name, :age 
+  
   def initialize(name="Unknow", age, parent_permission=true)
     @id = Random.rand(1..1000)
     @name = name
+    @parent_permission = parent_permission
   end
 end
