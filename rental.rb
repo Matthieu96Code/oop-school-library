5fa48e7309f4af49734d648ef226d552bd4b73ee
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-17 18:09:56 +0000

    Create relationship rental book

--- rental.rb
+++ rental.rb
@@ -1,10 +1,12 @@
 class Rental
   attr_accessor :date
-  attr_reader :person
+  attr_reader :person, :book
 
-  def initialize(date, person)
+  def initialize(date, person, book)
     @date = date
     @person = person
+    @book = book
     person.rentals << self
+    book.rentals << self
   end
 end
