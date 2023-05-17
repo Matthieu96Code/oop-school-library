42ec6176fc97a297ca3bb564f9a50b5f1aeb5397
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-17 09:24:17 +0000

    Prepare trimmer decorator

--- decorator.rb
+++ decorator.rb
@@ -17,3 +17,10 @@ class CapitalizeDecorator < Decorator
   end
 end
 
+class TrimmerDecorator < Decorator
+  def correct_name
+    trimmed_name = @nameable.correct_name[0, 10]
+    trimmed_name.length < @nameable.correct_name.length ? trimmed_name + '...' : trimmed_name
+  end
+end
+
