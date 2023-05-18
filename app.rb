05698569ff20247cc82759a7ca374f016c4a943a
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-18 23:55:01 +0000

    Add list all book method

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ class App
   def initialize
     @books = []
   end
+
+  def list_all_books
+    if @books.empty?
+      puts books empty
+      return
+    end
+    @books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
 end
