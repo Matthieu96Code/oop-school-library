3db35651433597d831d7e038950bc99e37b0703b
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-19 10:11:48 +0000

    refactor list all books method

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ class App
   end
 
   def list_all_books
-    if @books.empty?
-      puts books empty
-      return
-    end
-    @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}"
+    unless @books.empty?
+      @books.each do |book|
+        puts "Title: #{book.title}, Author: #{book.author}"
+      end
+    else
+      puts books store empty
     end
   end
 end
