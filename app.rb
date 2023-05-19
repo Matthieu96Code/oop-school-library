75e41e90cbafb724839beca6ad2d8b5249f4f450
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-19 10:18:47 +0000

    Create list all peple method

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 class App
   def initialize
     @books = []
+    @people = []
   end
 
   def list_all_books
@@ -10,6 +11,16 @@ class App
       end
     else
       puts books store empty
+    end
+  end
+
+  def list_all_people
+    unless @people.empty?
+      @people.each do |book|
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
+    else
+      puts there is none registered
     end
   end
 end
