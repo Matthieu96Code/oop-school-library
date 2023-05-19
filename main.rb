88baed6d20c451d894d8527e59be1134552c378e
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-19 16:18:35 +0000

    Add a loop for the main menu and functions

--- main.rb
+++ main.rb
@@ -5,34 +5,35 @@ require './app'
 def main
   app = App.new
   puts 'Welcome to School Library App!'
+  loop do
+    puts ''
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
 
-  puts ''
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
+    selected = gets.chomp
 
-  selected = gets.chomp
-
-  if selected == '1'
-    app.list_all_books
-  elsif selected == '2'
-    app.list_all_people
-  elsif selected == '3'
-    app.create_person
-  elsif selected == '4'
-    app.create_book
-  elsif selected == '5'
-    app.create_rental
-  elsif selected == '6'
-    app.list_all_rentals
-  elsif selected == '7'
-    puts 'thank you for using this app!'
-    return
+    if selected == '1'
+      app.list_all_books
+    elsif selected == '2'
+      app.list_all_people
+    elsif selected == '3'
+      app.create_person
+    elsif selected == '4'
+      app.create_book
+    elsif selected == '5'
+      app.create_rental
+    elsif selected == '6'
+      app.list_all_rentals
+    elsif selected == '7'
+      puts 'thank you for using this app!'
+      return
+    end
   end
 end
 
