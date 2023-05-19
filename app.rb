e94cb6195c7b9243d5fd046c44b235638faacfe3
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-19 11:52:58 +0000

    Fix basic linters error for spaces

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App
   end
 
   def list_all_books
-    unless @books.empty?
+    if !@books.empty?
       @books.each do |book|
         puts "Title: #{book.title}, Author: #{book.author}"
       end
@@ -21,7 +21,7 @@ class App
   end
 
   def list_all_people
-    unless @people.empty?
+    if !@people.empty?
       @people.each do |book|
         puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
@@ -34,31 +34,31 @@ class App
     puts 'Do you want to create a student (1) or a teacher(2)?....'
     person_choice = gets.chomp
 
-    if person_choice ===  '1'
+    if person_choice == '1'
       print 'Name:'
       name = gets.chomp
-  
+
       print 'Age:'
       age = gets.chomp.to_i
-  
+
       print 'Has Parent permission? [Y/N]:'
       parent_permission = gets.chomp.downcase
-  
+
       student = Student.new(@classroom, age, name, parent_permission: parent_permission == 'y')
       @people << student
       puts 'Student created successfully'
 
-    elsif person_choice ===  '2'
+    elsif person_choice == '2'
 
       print 'Name:'
       name = gets.chomp
-  
+
       print 'Age:'
       age = gets.chomp.to_i
-  
+
       print 'Specialization:'
       specialization = gets.chomp
-  
+
       @people << Teacher.new(age, specialization, name)
       puts 'Teacher created successfully'
 
